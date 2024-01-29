2a6a44f0c7f59a652dcfc95317b39ef1cc83d942
ilopjim13 <ljivan@iesrafaelalberti.es>
2024-01-29 21:27:10 +0100

    realizado en clase

--- src/ConjuntoLibros.kt
+++ src/ConjuntoLibros.kt
@@ -1,26 +1,29 @@
 class ConjuntoLibros {
-    var libros = Array(3) {Libro("", "", 0, 0)}
+    var libros = MutableList(3) {Libro("", "", 0, 0)}
 
     fun agregarLibros(titulo:String, autor:String, pag:Int, cal:Int) {
         if (this.libros[0].titulo == ""  && this.libros[1].titulo == "" && this.libros[2].titulo == "") this.libros[0] = Libro(titulo, autor, pag, cal)
         else if (this.libros[0].titulo != ""  && this.libros[1].titulo == "" && this.libros[2].titulo == "") this.libros[1] = Libro(titulo, autor, pag, cal)
         else if (this.libros[0].titulo != ""  && this.libros[1].titulo != "" && this.libros[2].titulo == "") this.libros[2] = Libro(titulo, autor, pag, cal)
-        else if (this.libros[0].titulo != ""  && this.libros[1].titulo != "" && this.libros[2].titulo != "")  println("Limite de cuentas alcanzado.")
+        else if (this.libros[0].titulo != ""  && this.libros[1].titulo != "" && this.libros[2].titulo != "")  println("Limite de libros alcanzado.")
     }
 
     fun eliminarLibros(elimina:String) {
         for (libro in libros) {
             if (libro.titulo == elimina && libro.autor == elimina) {
-                libros
+                libros.remove(libro)
             }
         }
     }
 
     fun mostrarCalificacion() {
-
+        println("El libro con mayor calificación es ${libros.maxBy { it.calificacion }}")
+        println("El libro con menor calificación es ${libros.minBy { it.calificacion }}")
     }
 
     fun mostrarContenido() {
-
+        for(libro in libros) {
+            println(libro)
+        }
     }
 }
